7291914824b70b57fbd2511d6089c6943be000e6
Manel Rodero <user.email=manelrodero@users.noreply.github.com>
2026-02-28 09:36:44 +0100

    Fix typo Component

--- Scripts/WriteToHKCUFromSystemWithLogs.ps1
+++ Scripts/WriteToHKCUFromSystemWithLogs.ps1
@@ -83,27 +83,27 @@ function Write-Registry {
     param($RegFileContents)
     $tempFile = '{0}{1:yyyyMMddHHmmssff}.reg' -f [IO.Path]::GetTempPath(), (Get-Date)
     $RegFileContents | Out-File -FilePath $tempFile
-    Write-Log ('Writing registry from file {0}' -f $tempFile) -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+    Write-Log ('Writing registry from file {0}' -f $tempFile) -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
     try { $p = Start-Process -FilePath C:\Windows\regedit.exe -ArgumentList "/s $tempFile" -PassThru -Wait } catch { }
     if ($null -ne $p) { $exitCode = $p.ExitCode } else { $exitCode = 0 }
     if ($exitCode -ne 0) {
-        Write-Log 'There was an error merging the reg file' -Level Warning -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+        Write-Log 'There was an error merging the reg file' -Level Warning -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
     } else {
         Remove-Item -Path $tempFile -Force -ErrorAction SilentlyContinue
     }
 }
 
 if (-not (Test-Path -Path $RegFile)) {
-    Write-Log "RegFile $RegFile doesn't exist. Operation aborted" -Level Error -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+    Write-Log "RegFile $RegFile doesn't exist. Operation aborted" -Level Error -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
 } else {
 
     if ($CurrentUser -or $AllUsers -or $DefaultProfile) {
 
-        Write-Log ('Reading the registry file {0}' -f $RegFile) -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+        Write-Log ('Reading the registry file {0}' -f $RegFile) -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
         $registryData = Get-Content -Path $RegFile -ReadCount 0
 
         if ($CurrentUser) {
-            Write-Log "Writing to the currenlty logged on user's registry" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+            Write-Log "Writing to the currenlty logged on user's registry" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
             $explorers = Get-WmiObject -Namespace root\cimv2 -Class Win32_Process -Filter "Name='explorer.exe'"
             $explorers | ForEach-Object {
                 $owner = $_.GetOwner()
@@ -118,20 +118,20 @@ if (-not (Test-Path -Path $RegFile)) {
         }
 
         if ($AllUsers) {
-            Write-Log "Writing to every user's registry" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+            Write-Log "Writing to every user's registry" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
             $res = C:\Windows\system32\reg.exe query HKEY_USERS
             $res -notmatch 'S-1-5-18|S-1-5-19|S-1-5-20|DEFAULT|Classes' | ForEach-Object {
                 if ($_ -ne '') {
                     $sid = $_ -replace 'HKEY_USERS\\'
                     $RegFileContents = $registryData -replace 'HKEY_CURRENT_USER', "HKEY_USERS\$sid"
-                    Write-Log "- $sid" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+                    Write-Log "- $sid" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
                     Write-Registry -RegFileContents $RegFileContents
                 }
             }
         }
 
         if ($DefaultProfile) {
-            Write-Log "Writing to the default profile's registry (for future users)" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+            Write-Log "Writing to the default profile's registry (for future users)" -Level Info -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
             C:\Windows\System32\reg.exe load 'HKU\DefaultUser' C:\Users\Default\NTUSER.DAT | Out-Null
             $RegFileContents = $registryData -replace 'HKEY_CURRENT_USER', 'HKEY_USERS\DefaultUser'
             Write-Registry -RegFileContents $RegFileContents
@@ -139,6 +139,6 @@ if (-not (Test-Path -Path $RegFile)) {
         }
 
     } else {
-        Write-Log 'No mode was selected. Operation aborted' -Level Warning -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystem"
+        Write-Log 'No mode was selected. Operation aborted' -Level Warning -UseCMTrace -LogName "LogName" -Component "WriteToHKCUFromSystemWithLogs"
     }
 }
